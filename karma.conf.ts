3467c5b77a38da6a0b680e8bb75d1c623c49fef7
Oscar Rieken <oscar.rieken@adp.com>
2018-02-01 15:15:27 -0500

    removed zone.js was not used

--- karma.conf.ts
+++ karma.conf.ts
@@ -7,13 +7,6 @@ module.exports = (config: any) => {
     browsers: ['ChromeHeadless'],
     colors: true,
     files: [
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
       { pattern: 'lib/**/*.ts' }
     ],
     frameworks: ['jasmine', 'karma-typescript'],

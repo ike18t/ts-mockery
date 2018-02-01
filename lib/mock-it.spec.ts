b0a460468cdfe5dafd63762170cae71dc5dc6d79
Isaac Datlof <ike18t@gmail.com>
2018-02-01 10:55:21 -0500

    feat: add ability to extend object

--- lib/mock-it.spec.ts
+++ lib/mock-it.spec.ts
@@ -1,13 +1,33 @@
 import { MockIt } from './mock-it';
 
 describe('MockIt', () => {
-  it('should do stuff', () => {
-    class Bah {
-      foo: string;
-      bar = (buzz: string) => buzz.toUpperCase();
-    }
-    const hi = MockIt<Bah>({ bar: () => 'hi' });
-    hi.bar('bye');
-    expect(hi.bar).toHaveBeenCalledWith('bye');
+  class Bah {
+    foo: string;
+    bar = (buzz: string) => buzz.toUpperCase();
+  }
+
+  it('should mock the supplied method', () => {
+    const mock = MockIt<Bah>({ bar: () => 'hi' });
+    const result = mock.bar('bye');
+    expect(mock.bar).toHaveBeenCalledWith('bye');
+    expect(result).toEqual('hi');
+  });
+
+  it('should allow you to overwrite preexisting mock', () => {
+    const mock = MockIt<Bah>({ bar: () => 'hi' });
+    MockIt<Bah>(mock, { bar: () => 'foooo' });
+    const result = mock.bar('bye');
+    expect(result).toEqual('foooo');
+  });
+
+  it('should allow you to use an empty constructor', () => {
+    expect(() => MockIt<Bah>()).not.toThrow(); /* tslint:disable-line:no-unnecessary-callback-wrapper */
+  });
+
+  it('should allow you to use an empty constructor', () => {
+    const mock = MockIt<Bah>();
+    MockIt<Bah>(mock, { bar: () => 'foooo' });
+    const result = mock.bar('bye');
+    expect(result).toEqual('foooo');
   });
 });
--- lib/mock-it.ts
+++ lib/mock-it.ts
@@ -1,7 +1,14 @@
-export function MockIt<T>(object: Partial<{ [ key in keyof T ]: T[key] }>): T {
-  Object.keys(object).forEach((key) => {
-    spyOn(object, key as keyof T);
+export function MockIt<T>(object?: Partial<{ [ key in keyof T ]: T[key] }>,
+                          extension?: Partial<{ [ key in keyof T ]: T[key] }>): T {
+  const spyTarget = extension || object;
+  if (!spyTarget) {
+    return {} as T; // tslint:disable-line:no-object-literal-type-assertion
+  }
+
+  Object.keys(spyTarget).forEach((key) => {
+    spyOn(spyTarget, key as keyof T).and.callThrough();
   });
+  Object.assign(object, spyTarget);
   return object as T;
 }
 

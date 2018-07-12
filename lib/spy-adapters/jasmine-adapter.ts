e33f54bb61d9165378f8a78f8249862f6fa9e8b6
Isaac Datlof <ike18t@gmail.com>
2018-07-12 09:55:22 -0400

    refactor: lint fix

--- lib/spy-adapters/jasmine-adapter.ts
+++ lib/spy-adapters/jasmine-adapter.ts
@@ -8,14 +8,16 @@ export class JasmineAdapter implements SpyAdapter {
   // tslint:disable-next-line:ban-types
   spyAndCallFake<T, K extends keyof T>(object: T, key: K, stub: T[K] & Function) {
     const value = object[key];
-    const spy = ((jasmine as any).isSpy(value) ? value : spyOn(object, key)) as jasmine.Spy; // tslint:disable-line
+    // tslint:disable-next-line:no-unsafe-any
+    const spy = ((jasmine as any).isSpy(value) ? value : spyOn(object, key)) as jasmine.Spy;
     spy.calls.reset();
     spy.and.callFake(stub);
   }
 
   spyAndCallThrough<T, K extends keyof T>(object: T, key: K) {
     const value = object[key];
-    if (typeof value === 'function' && !(jasmine as any).isSpy(value)) { // tslint:disable-line
+    // tslint:disable-next-line:no-unsafe-any
+    if (typeof value === typeof Function && !(jasmine as any).isSpy(value)) {
       spyOn(object, key).and.callThrough();
     }
   }
